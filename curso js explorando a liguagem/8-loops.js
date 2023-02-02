c017e1c2257b5decad47b81733a4b1b6eb2b216b
Bianca <119738231+biancabiazin@users.noreply.github.com>
2023-02-02 13:14:10 -0300

    Add files via upload

---
+++ curso js explorando a liguagem/8-loops.js
@@ -0,0 +1,44 @@
+console.log(`\n Trabalhando com Condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+);
+
+const idadeComprador = 18;
+const estaAcompanhada = true;
+let temPassagemComprada = false;
+const destino = "Rio de Janeiro";
+
+
+console.log("\n Destinos posséveis:");
+console.log(listaDeDestinos);
+
+
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
+
+let contador = 0;
+let destinoExiste = false
+while(contador<3){
+    if(listaDeDestinos[contador] == destino){
+        console.log("Destino existe");
+        destinoExiste = true;
+        break;
+    }
+    contador +=1;
+}
+
+console.log("Destino existe: ", destinoExiste);
+
+if(podeComprar && destinoExiste) {
+    console.log("Boa viagem");
+}else{
+    console.log("Desculpe tivemos um erro");
+}
+
+for(let cont = 0 ; cont < 3; cont++){
+    if(listaDeDestinos[contador] == destino){
+        destinoExiste = true;
+    }
+}
